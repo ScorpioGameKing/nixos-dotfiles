d938285831486506c9ece5d687e88d71a8ca85d5
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-16 01:19:25 -0400

    adding stylix, organizing code, enabling nvim marker folding as test

--- configuration.nix
+++ configuration.nix
@@ -1,33 +1,87 @@
+# -------------------------
+# {{{ Scorpio's Nix-Box Configs
+# -------------------------
+
 { config, lib, pkgs, ... }:
 
 {
+  # -------------------------
+  # {{{ Nix Settings
+  # -------------------------
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "26.05";
+  # -------------------------
+  # }}}
+  # -------------------------
+
+  # -------------------------
+  # {{{ Flake imports
+  # -------------------------
   imports =
     [ 
       ./hardware-configuration.nix
     ];
+  # -------------------------
+  # }}}
+  # -------------------------
 
+  # -------------------------
+  # {{{ System Wide Package Installs
+  # -------------------------
+  environment.systemPackages = with pkgs; [
+    wget         # 
+    cmake        # Build C Stuff
+    gcc          # Compile C Stuff
+    nodejs       # We have this here for neovim plugins(?)
+    ripgrep      # Grep go BRRRRRR
+    tree-sitter  # I like syntax-highlighting anywhere possible
+    eza          # Better ls
+    zoxide       # Better cd
+    fzf          # Fuzzy find files in the terminal
+    pinentry-all # Current Solution to sign with git
+  ];
+  # -------------------------
+  # }}}
+  # -------------------------
+
+  # -------------------------
+  # {{{ Set up the system-wide font, Agave is nice.
+  # -------------------------
+  fonts.packages = with pkgs; [
+    nerd-fonts.agave
+  ];
+  # -------------------------
+  # }}}
+  # -------------------------
+ 
+  # -------------------------
+  # {{{ System Config Options
+  # -------------------------
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nix-box"; 
-
   networking.networkmanager.enable = true;
 
-  time.timeZone = "America/New_York";
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
 
-  programs.niri = {
-    enable = true;
-  };
   services.displayManager.ly.enable = true;
-
   services.printing.enable = true;
-
   services.libinput.enable = true;
-
+  
   security.pam.services.swaylock = {};
 
+  time.timeZone = "America/New_York";
+  # -------------------------
+  # }}}
+  # -------------------------
+
+  # -------------------------
+  # {{{ Create our user
+  # -------------------------
   users.users.scorpio= {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
@@ -35,14 +89,16 @@
       tree
     ];
   };
+  # -------------------------
+  # }}}
+  # -------------------------
 
-  nixpkgs.config.allowUnfree = true;
-
-  programs.xwayland.enable = true;
-  programs.gamemode.enable = true;
-
-  hardware.graphics.enable = true;
-  hardware.graphics.enable32Bit = true;
+  # -------------------------
+  # {{{ Enable packages at system level
+  # -------------------------
+  programs.niri = {
+    enable = true;
+  };
 
   programs.steam = {
     enable = true;
@@ -52,26 +108,10 @@
       proton-ge-bin
     ];
   };
-
-  environment.systemPackages = with pkgs; [
-    vim 
-    wget
-    cmake
-    gcc
-    nodejs
-    ripgrep
-    tree-sitter
-    eza
-    zoxide
-    fzf
-    pinentry-all
-  ];
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.agave
-  ];
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  system.stateVersion = "26.05";
+  # -------------------------
+  # }}}
+  # -------------------------
 }
-
+# -------------------------
+# }}}
+# -------------------------
--- flake.nix
+++ flake.nix
@@ -1,28 +1,33 @@
 {
-    description = "NixOS from Scratch";
-    inputs = {
-        nixpkgs.url = "nixpkgs/nixos-26.05";
-	home-manager = {
-            url = "github:nix-community/home-manager/release-26.05";
-	    inputs.nixpkgs.follows = "nixpkgs";
-        };
+  description = "NixOS from Scratch";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-26.05";
+    stylix = {
+      url = "github:nix-community/stylix/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+	    inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-    outputs = { self, nixpkgs, home-manager, ... }: {
-	nixosConfigurations.nix-box = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, stylix, home-manager, ... }: {
+	  nixosConfigurations.nix-box = nixpkgs.lib.nixosSystem {
 	    system = "x86_64-linux";
 	    modules = [
-	        ./configuration.nix
-		home-manager.nixosModules.home-manager
-		{
-		    home-manager = {
+        stylix.nixosModules.stylix
+	      ./configuration.nix
+		    home-manager.nixosModules.home-manager
+		    {
+		      home-manager = {
 		        useGlobalPkgs = true;
-			useUserPackages = true;
-			users.scorpio = import ./home.nix;
-			backupFileExtension = "backup";
-		    };
-		}
-	    ];
-	};
-    };
+			      useUserPackages = true;
+			      users.scorpio = import ./home.nix;
+			      backupFileExtension = "backup";
+		      };
+		    }
+      ];
+	  };
+  };
 }
