49ea997e2bee2f90b8d0cb579ac83c7187895be8
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-16 02:07:32 -0400

    Squash merge sharded into main

--- configuration.nix
+++ configuration.nix
@@ -52,6 +52,7 @@
   # -------------------------
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 20;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nix-box"; 
