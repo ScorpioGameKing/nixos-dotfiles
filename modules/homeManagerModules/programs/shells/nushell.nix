5b6090200bc71b8b7726fc6da5a5055ccef5a2b6
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-17 11:12:26 -0400

    && to ; for nushell

--- modules/homeManagerModules/programs/shells/nushell.nix
+++ modules/homeManagerModules/programs/shells/nushell.nix
@@ -14,7 +14,7 @@
         
         # General Shorthands
         btw = "echo I use NixOS, btw";
-        ff = "clear && fastfetch";
+        ff = "clear ; fastfetch";
         ls = "eza -TF -L 1 -a -s type --icons=auto -lUmh --git-repos --no-permissions";
         vi = "nvim";
 
@@ -23,12 +23,12 @@
         qnud = "nix flake update niri-dotfiles-repo --flake ~/nixos-dotfiles";
         
         # Nix-Box Build Commands
-        qnrb = "lg -p ~/nixos-dotfiles && sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nix-box";
-        qnrbd = "qnud && lg -p ~/nixos-dotfiles && sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nix-box";
-        qnrbf = "qnc && qnud && lg -p ~/nixos-dotfiles && sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nix-box";
+        qnrb = "lg -p ~/nixos-dotfiles ; sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nix-box";
+        qnrbd = "qnud ; lg -p ~/nixos-dotfiles ; sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nix-box";
+        qnrbf = "qnc ; qnud ; lg -p ~/nixos-dotfiles ; sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nix-box";
 
       };
-      
+
       extraLogin = ''
         ff
       '';
