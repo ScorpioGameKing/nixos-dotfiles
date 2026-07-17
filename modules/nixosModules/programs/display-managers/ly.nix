406f26563a183d5d342efe2ac2b4dfeb31ed1618
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-17 09:18:06 -0400

    testing another method

--- modules/nixosModules/programs/display-managers/ly.nix
+++ modules/nixosModules/programs/display-managers/ly.nix
@@ -1,4 +1,10 @@
-{ lib, config, ... }: {
+{ lib, config, ... }: 
+
+let 
+  hostname = if (builtins.pathExists ./private.nix) then (import ./private.nix) else  "NixOS"; 
+in
+
+{
 
   options = {
     ly.enable = lib.mkEnableOption "enables ly";
@@ -9,7 +15,7 @@
       enable = true;
       settings = {
         animation = "matrix";
-        box_title = "${networking.hostname}";
+        box_title = "${hostname}";
         corner_top_left = "shutdown,restart,britup,britdown,password,tty";
         corner_top_right = "clock,battery,numlock,capslock";
       };
