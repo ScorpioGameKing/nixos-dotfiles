07de36293819ea795f91917fa16718f568c0974d
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-17 09:32:22 -0400

    further ly configs

--- modules/nixosModules/programs/display-managers/ly.nix
+++ modules/nixosModules/programs/display-managers/ly.nix
@@ -14,7 +14,11 @@ in
     services.displayManager.ly = {
       enable = true;
       settings = {
-        animation = "matrix";
+        animation = "colormix";
+        cmatrix_min_codepoint = "0x3000";
+        cmatrix_max_codepoint = "0x30FF";
+        clock = "%I:%M %p";
+        vi_mode = true;
         box_title = "${hostname}";
         corner_top_left = "shutdown,restart,britup,britdown,password,tty";
         corner_top_right = "clock,battery,numlock,capslock";
