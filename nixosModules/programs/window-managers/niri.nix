559a755a3661a0768649eb563a501bde30cc3c8b
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-17 01:48:21 -0400

    removed, solution is simple?

--- nixosModules/programs/window-managers/niri.nix
+++ nixosModules/programs/window-managers/niri.nix
@@ -1,20 +1,10 @@
 { lib, config, inputs, ... }: 
 
-let
-  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
-  inherit (inputs) niri-dotfiles-repo;
-in
-
 {
 
   options = {
     niri.enable = lib.mkEnableOption "enables niri";
   };
-
-  xdg.configFile.niri = {
-      source = create_symlink "${niri-dotfiles-repo}/.config/niri";
-      recursive = true;
-    };
   
   config = lib.mkIf config.niri.enable {
     programs.niri = {
