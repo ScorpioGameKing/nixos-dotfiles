470327ffa6c6bf17c8a24a2d93893a8b1526c622
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-17 14:52:54 -0400

    fixes

--- modules/nixosModules/programs/tools/python314Packages-colorama.nix
+++ modules/nixosModules/programs/tools/python314Packages-colorama.nix
@@ -1,10 +1,10 @@
 { pkgs, lib, config, ... }: {
 
   options = {
-    python314Packages.colorama = lib.mkEnableOption "enables python314Packages.colorama";
+    python314Packages-colorama = lib.mkEnableOption "enables python314Packages.colorama";
   };
   
-  config = lib.mkIf config.python3.enable {
+  config = lib.mkIf config.python314Packages-colorama.enable {
     environment.systemPackages = with pkgs; [
       python314Packages.colorama
     ];
--- modules/nixosModules/programs/tools/python314Packages-raylib-python-cffi.nix
+++ modules/nixosModules/programs/tools/python314Packages-raylib-python-cffi.nix
@@ -1,10 +1,10 @@
 { pkgs, lib, config, ... }: {
 
   options = {
-    python314Packages.raylib-python-cffi.enable = lib.mkEnableOption "enables python314Packages.raylib-python-cffi (raylib for python 314)";
+    python314Packages-raylib-python-cffi.enable = lib.mkEnableOption "enables python314Packages.raylib-python-cffi (raylib for python 314)";
   };
   
-  config = lib.mkIf config.python3.enable {
+  config = lib.mkIf config.python314Packages-raylib-python-cffi.enable {
     environment.systemPackages = with pkgs; [
       python314Packages.raylib-python-cffi
     ];
