90466fc28286862a5ce4d75794ed4fb965215b7d
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-18 19:35:12 -0400

    import path fixed

--- modules/nixosModules/programs/display-managers/ly.nix
+++ modules/nixosModules/programs/display-managers/ly.nix
@@ -8,16 +8,16 @@
     services.displayManager.ly = {
       enable = true;
       settings = {
-        animation = config.nixos-modules.display-managers.ly.animation;
-        cmatrix_min_codepoint = config.nixos-modules.display-managers.ly.cmatrix_min_codepoint;
-        cmatrix_max_codepoint = config.nixos-modules.display-managers.ly.cmatrix_max_codepoint;
-        colormix_col1 = config.nixos-modules.display-managers.ly.colormix_col1;
-        colormix_col2 = config.nixos-modules.display-managers.ly.colormix_col2;
-        colormix_col3 = config.nixos-modules.display-managers.ly.colormix_col3;
-        clock = config.nixos-modules.display-managers.ly.clock;
-        box_title = config.nixos-modules.display-managers.ly.box_title;
-        corner_top_left = config.nixos-modules.display-managers.ly.corner_top_left;
-        corner_top_right = config.nixos-modules.display-managers.ly.corner_top_right;
+        animation = config.nixos-modules.display-managers.ly.settings.animation;
+        cmatrix_min_codepoint = config.nixos-modules.display-managers.ly.settings.cmatrix_min_codepoint;
+        cmatrix_max_codepoint = config.nixos-modules.display-managers.ly.settings.cmatrix_max_codepoint;
+        colormix_col1 = config.nixos-modules.display-managers.ly.settings.colormix_col1;
+        colormix_col2 = config.nixos-modules.display-managers.ly.settings.colormix_col2;
+        colormix_col3 = config.nixos-modules.display-managers.ly.settings.colormix_col3;
+        clock = config.nixos-modules.display-managers.ly.settings.clock;
+        box_title = config.nixos-modules.display-managers.ly.settings.box_title;
+        corner_top_left = config.nixos-modules.display-managers.ly.settings.corner_top_left;
+        corner_top_right = config.nixos-modules.display-managers.ly.settings.corner_top_right;
       };
     };
   };
