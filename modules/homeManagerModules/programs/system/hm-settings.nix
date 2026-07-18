3508274eecacc01bc2c0595c21d3a6f4ff95869c
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-17 23:52:12 -0400

    seriously don't touch it

--- modules/homeManagerModules/programs/system/hm-settings.nix
+++ modules/homeManagerModules/programs/system/hm-settings.nix
@@ -5,6 +5,5 @@
   config = lib.mkIf config.hm-modules.system.hm-settings.enable {
       home.username = config.hm-modules.system.hm-settings.username;
       home.homeDirectory = config.hm-modules.system.hm-settings.homeDirectory;
-      home.stateVersion = "26.05";
   };
 }
