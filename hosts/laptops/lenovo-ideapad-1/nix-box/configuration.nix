aa220effe81427a5cb13c0b038e47c66ca67b591
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-18 19:31:47 -0400

    exposing ly options

--- hosts/laptops/lenovo-ideapad-1/nix-box/configuration.nix
+++ hosts/laptops/lenovo-ideapad-1/nix-box/configuration.nix
@@ -22,8 +22,10 @@
 
   nixos-modules = {
     enable = true;
+
     desktop = {
       enable = true;
+
       nb-stylix = {
         enable = true;
         base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
@@ -31,16 +33,36 @@
       };
       xwayland-satellite.enable = true;
     };
+
     display-managers = {
       enable = true;
-      ly.enable = true;
+
+      ly - {
+        enable = true;
+        settings = {
+          animation = "colormix";
+          cmatrix_min_codepoint = "0x3000";
+          cmatrix_max_codepoint = "0x30FF";
+          colormix_col1 = "0x00282828";
+          colormix_col2 = "0x00504945";
+          colormix_col3 = "0x00EBDBB2";
+          clock = "%I:%M %p";
+          box_title = "NixOS";
+          corner_top_left = "shutdown,restart,britup,britdown,password,tty";
+          corner_top_right = "clock,battery,numlock,capslock";
+        };
+      };
     };
+
     gui-apps = {
       enable = true;
+
       steam.enable = true;
     };
+
     system = {
       enable = true;
+
       boot.enable = true;
       font.enable = true;
       hardware.enable = true;
@@ -50,8 +72,10 @@
       services.enable = true;
       time.enable = true;
     };
+
     tools = {
       enable = true;
+
       cmake.enable = true;
       gcc.enable = true;
       nodejs.enable = true;
@@ -63,12 +87,16 @@
       tree-sitter.enable = true;
       wget.enable = true;
     };
+
     users = {
       enable = true;
+
       scorpio.enable = true;
     };
+
     window-managers = {
       enable = true;
+
       niri.enable = true;
     };
   };
--- modules/nixosModules/programs/display-managers/ly.nix
+++ modules/nixosModules/programs/display-managers/ly.nix
@@ -8,16 +8,16 @@
     services.displayManager.ly = {
       enable = true;
       settings = {
-        animation = "colormix";
-        cmatrix_min_codepoint = "0x3000";
-        cmatrix_max_codepoint = "0x30FF";
-        colormix_col1 = "0x00282828";
-        colormix_col2 = "0x00504945";
-        colormix_col3 = "0x00EBDBB2";
-        clock = "%I:%M %p";
-        box_title = "NixOS";
-        corner_top_left = "shutdown,restart,britup,britdown,password,tty";
-        corner_top_right = "clock,battery,numlock,capslock";
+        animation = config.nixos-modules.display-managers.ly.animation;
+        cmatrix_min_codepoint = config.nixos-modules.display-managers.ly.cmatrix_min_codepoint;
+        cmatrix_max_codepoint = config.nixos-modules.display-managers.ly.cmatrix_max_codepoint;
+        colormix_col1 = config.nixos-modules.display-managers.ly.colormix_col1;
+        colormix_col2 = config.nixos-modules.display-managers.ly.colormix_col2;
+        colormix_col3 = config.nixos-modules.display-managers.ly.colormix_col3;
+        clock = config.nixos-modules.display-managers.ly.clock;
+        box_title = config.nixos-modules.display-managers.ly.box_title;
+        corner_top_left = config.nixos-modules.display-managers.ly.corner_top_left;
+        corner_top_right = config.nixos-modules.display-managers.ly.corner_top_right;
       };
     };
   };
