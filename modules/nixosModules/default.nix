b9d11733fa8f8147eaf3cd781158e7664d9467ab
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-20 07:13:42 -0400

    you know? I don't actually use libre anyways

--- modules/nixosModules/default.nix
+++ modules/nixosModules/default.nix
@@ -121,13 +121,6 @@
       type = lib.types.submodule {
         options = {
           enable = lib.mkEnableOption "enables gui-apps";
-          libreoffice = lib.mkOption {
-            type = lib.types.submodule {
-              options = {
-                enable = lib.mkEnableOption "enables libreoffice";
-              };
-            };
-          };
           steam = lib.mkOption {
             type = lib.types.submodule {
               options = {
--- modules/nixosModules/programs/gui-apps/libreoffice.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, lib, config, ... }: {
-  config = lib.mkIf config.nixos-modules.gui-apps.libreoffice.enable {
-    environment.systemPackages = with pkgs; [
-      libreoffice
-    ];
-  };
-}
