120b8360467bb3a05397745ab6da896d10c6c581
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-20 08:05:13 -0400

    typo

--- modules/nixosModules/programs/tools/pinentry-all.nix
+++ modules/nixosModules/programs/tools/pinentry-all.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, config, ... }: {=
+{ pkgs, lib, config, ... }: {
   config = lib.mkIf config.nixos-modules.tools.pinentry-all.enable {
     environment.systemPackages = with pkgs; [
       pinentry-all
