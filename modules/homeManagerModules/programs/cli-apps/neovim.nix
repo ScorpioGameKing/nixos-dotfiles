87b6f374d01c90472814991859831371dd60687b
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-21 10:27:26 -0400

    test

--- modules/homeManagerModules/programs/cli-apps/neovim.nix
+++ modules/homeManagerModules/programs/cli-apps/neovim.nix
@@ -2,10 +2,9 @@
 let
   create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
   inherit (inputs) nixos-dotfiles-repo;
-  inherit (inputs) nvf;
 in
 {
-  import = [ nvf.homeManagerModules.default ];
+  import = [ inputs.nvf.homeManagerModules.default ];
   config = lib.mkIf config.hm-modules.cli-apps.neovim.enable {
     home.packages = with pkgs; [
       neovim
