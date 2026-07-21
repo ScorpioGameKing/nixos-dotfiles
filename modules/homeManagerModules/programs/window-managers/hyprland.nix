7ac810c9488c78794aa89a8cf526cb73b5581eb0
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-21 18:40:08 -0400

    updated hyprland dots and config

--- modules/homeManagerModules/programs/window-managers/hyprland.nix
+++ modules/homeManagerModules/programs/window-managers/hyprland.nix
@@ -1,5 +1,14 @@
-{ lib, config, inputs, ... }: {
+{ lib, config, inputs, ... }: 
+let
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+  inherit (inputs) nixos-dotfiles-repo;
+in
+{
   config = lib.mkIf config.hm-modules.window-managers.hyprland.enable {
+    xdg.configFile.niri = {
+      source = create_symlink "${nixos-dotfiles-repo}/.config/hypr";
+      recursive = true;
+    };
     wayland.windowManager.hyprland = {
       enable = true;
       package = null;
