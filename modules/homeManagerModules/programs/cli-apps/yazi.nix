3616673dd6944aec913b3cf89ce3aab7578d90d2
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-21 11:16:49 -0400

    nor will this

--- modules/homeManagerModules/programs/cli-apps/yazi.nix
+++ modules/homeManagerModules/programs/cli-apps/yazi.nix
@@ -1,17 +1,23 @@
 { pkgs, lib, config, inputs, ...  }: 
 let
   create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
-  inherit (inputs) nixos-dotfiles-repo;
-  if config.hm-modules.useLocalRepo
-  then repo_path = "${config.hm-modules.localRepoPath}"
-  else repo_path = "${nixos-dotfiles-repo}"; 
+  inherit (inputs) nixos-dotfiles-repo; 
 in
 {
   config = lib.mkIf config.hm-modules.cli-apps.yazi.enable {
-    xdg.configFile.yazi = {
-      source = create_symlink "${repo_path}/.config/yazi/";
+    if config.hm-modules.useLocalRepo
+    then xdg.configFile.yazi = {
+      source = create_symlink "${config.hm-modules.localRepoPath}/.config/yazi/";
       recursive = true;
-    }; 
+    };
+    else xdg.configFile.yazi = {
+      source = create_symlink "${nixos-dotfiles-repo}/.config/yazi/";
+      recursive = true;
+    };
+    #xdg.configFile.yazi = {
+    #  source = create_symlink "${repo_path}/.config/yazi/";
+    #  recursive = true;
+    #}; 
     programs.yazi = {
       enable = true;
       enableBashIntegration = config.hm-modules.shells.bash.enable;
