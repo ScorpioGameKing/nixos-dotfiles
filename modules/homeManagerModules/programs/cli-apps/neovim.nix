50c37a942d37b7c35d26fd76e3812e67aa11dacf
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-21 10:53:17 -0400

    old configs back

--- modules/homeManagerModules/programs/cli-apps/neovim.nix
+++ modules/homeManagerModules/programs/cli-apps/neovim.nix
@@ -5,14 +5,12 @@ let
 in
 {
   config = lib.mkIf config.hm-modules.cli-apps.neovim.enable {
-    #home.packages = with pkgs; [
-    #  neovim
-    #];
-    #xdg.configFile.nvim = {
-    #  source = create_symlink "${nixos-dotfiles-repo}/.config/nvim";
-    #  recursive = true;
-    #};
-
-    
+    home.packages = with pkgs; [
+      neovim
+    ];
+    xdg.configFile.nvim = {
+      source = create_symlink "${nixos-dotfiles-repo}/.config/nvim";
+      recursive = true;
+    };
   };
 }
