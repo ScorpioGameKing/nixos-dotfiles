e1d149d7daeca560b695b191a775117592ca2523
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-21 08:41:27 -0400

    more bash alias toggles

--- modules/homeManagerModules/programs/shells/bash.nix
+++ modules/homeManagerModules/programs/shells/bash.nix
@@ -11,8 +11,8 @@ in
         
         # General Shorthands
         btw = "echo I use NixOS, btw";
-        ff = "clear && fastfetch";
-        ls = "eza -TF -L 1 -a -s type --icons=auto -lUmh --git-repos --no-permissions";
+        ff = lib.mkIf config.hm-modules.tools.fastfetch.enable "clear && fastfetch";
+        ls = lib.mkIf config.hm-modules.tools.eza.enable "eza -TF -L 1 -a -s type --icons=auto -lUmh --git-repos --no-permissions";
         vi = "nvim";
 
         jo = lib.mkIf config.hm-modules.cli-apps.joshuto.enable "joshuto";
